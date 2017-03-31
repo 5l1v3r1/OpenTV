2b5ba14e2e413e4f9e480d9ccdf711fd63f89e4b
Marplex <marcocimolai49@gmail.com>
2017-03-31 17:55:40 +0200

    Add left toolbar and dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('sidenav') sideNav;
+  menu: any;
+  sidenavOpened: boolean;
+
+  constructor() {
+    this.menu = [
+      {
+        icon: "receipt",
+        title: "News",
+        bottom: false
+      },
+      {
+        icon: "live_tv",
+        title: "Serie TV",
+        bottom: false
+      },
+      {
+        icon: "tv",
+        title: "Show televisivi",
+        bottom: false
+      },
+      {
+        icon: "theaters",
+        title: "Film",
+        bottom: false
+      },
+      {
+        icon: "add",
+        title: "Aggiungi un canale",
+        bottom: false
+      },
+      {
+        icon: "extension",
+        title: "Estensioni",
+        bottom: true
+      }
+    ];
+
+    this.sidenavOpened = false;
+  }
+
+  toggleSidenav() {
+    this.sideNav.nativeElement.setAttribute('style', 'width:'+ ( this.sidenavOpened ? '60px' : '250px') +' !important');
+    this.sidenavOpened = !this.sidenavOpened;
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -13,8 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MaterialModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
